0d5222310713e423ab6c9db70c9cd7d5cf738026
Chris Helm <christopher.helm@gmail.com>
2017-04-24 11:40:21 -0600

    trying to get tests to pass

--- tests/unit/test_ipe_image.py
+++ tests/unit/test_ipe_image.py
@@ -14,6 +14,7 @@ import tempfile
 import unittest
 import rasterio
 import dask.array as da
+from gbdxtools.ipe.io import to_geotiff
 
 try:
     from urlparse import urlparse
@@ -42,9 +43,9 @@ class IpeImageTest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        mock_gbdx_session = get_mock_gbdx_session(token='dymmytoken')
-        cls.gbdx = Interface(gbdx_connection=mock_gbdx_session)
-        #cls.gbdx = Interface()
+        #mock_gbdx_session = get_mock_gbdx_session(token='dymmytoken')
+        #cls.gbdx = Interface(gbdx_connection=mock_gbdx_session)
+        cls.gbdx = Interface()
         cls._temp_path = tempfile.mkdtemp()
         print("Created: {}".format(cls._temp_path))
 
@@ -73,14 +74,20 @@ class IpeImageTest(unittest.TestCase):
         assert img.shape == (8, 4514, 8135)
         assert img._proj == 'EPSG:3857' 
 
-    @my_vcr.use_cassette('tests/unit/cassettes/test_ipe_image_with_aoi.yaml', filter_headers=['authorization'])
+    #@my_vcr.use_cassette('tests/unit/cassettes/test_ipe_image_with_aoi.yaml', filter_headers=['authorization'])
     def test_ipe_image_aoi(self):
-        idahoid = '1ec49348-8950-49ff-bd71-ea2e4d8754ac'
+        #idahoid = '1ec49348-8950-49ff-bd71-ea2e4d8754ac'
+        idahoid = '42c9467f-9f01-4bb0-a7db-d25b614bb1a0'
+        bbox=[-104.9450969696045,39.79534795320513,-104.93994712829591,39.79729335524221]
         img = self.gbdx.idaho_image(idahoid)
-        aoi = img.aoi(bbox=[-74.01626586914064,45.394592696926615,-73.91601562500001,45.43363548747066])
-        assert aoi.shape == (8, 3169, 8135)
-        rgb = aoi[[4,2,1], ...]
-        assert isinstance(rgb, da.Array)
+        #aoi = img.aoi(bbox=[-74.01626586914064,45.394592696926615,-73.91601562500001,45.43363548747066])
+        aoi = img.aoi(bbox=bbox)
+        assert aoi.shape == (1, 440, 1164)#(8, 3169, 8135)
+        #rgb = aoi[[4,2,1], ...]
+        #assert isinstance(rgb, da.Array)
+        tif = to_geotiff(img, path='/tmp/output.tif')
+        with rasterio.open(tif) as src:
+            assert list(src.bounds) == bbox
 
     @my_vcr.use_cassette('tests/unit/cassettes/test_ipe_image_vrt.yaml', filter_headers=['authorization'])
     def test_ipe_image_vrt(self):
@@ -90,3 +97,14 @@ class IpeImageTest(unittest.TestCase):
         assert img.shape == (8, 4514, 8135)
         assert img._proj == 'EPSG:3857'
         assert isinstance(img.vrt, str)
+    
+    #@my_vcr.use_cassette('tests/unit/cassettes/test_ipe_image_with_aoi.yaml', filter_headers=['authorization'])
+    #def test_ipe_image_to_geotiff(self):
+    #    gbdx = Interface()
+    #    idahoid = '42c9467f-9f01-4bb0-a7db-d25b614bb1a0'
+    #    bbox=[-104.9450969696045,39.79534795320513,-104.93994712829591,39.79729335524221]
+    #    img = gbdx.idaho_image(idahoid, bbox=bbox)
+    #    print img.shape
+    #    tif = to_geotiff(img, path='/tmp/output.tif')
+    #    with rasterio.open(tif) as src:
+    #        assert list(src.bounds) == bbox
